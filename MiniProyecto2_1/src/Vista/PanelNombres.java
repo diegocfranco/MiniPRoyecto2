aea2a88d5b6e35d58199b961c0427f4f5b8fa2f1
diegocfranco <juan.diego.castano@correounivalle.edu.co>
2023-05-12 19:20:59 -0500

    inicio de proyecto y copia

---
+++ MiniProyecto2_1/src/Vista/PanelNombres.java
@@ -0,0 +1,227 @@
+package Vista;
+
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+
+
+
+
+public final class PanelNombres extends JFrame {
+    
+    
+    private PanelConImagen jpFondoNombres;
+    private JButton btnIniciar;
+    private JLabel lblNumeroRondas;
+    private JButton btnRonda1;
+    private JButton btnRonda2;
+    private JButton btnRonda3;
+    private JButton btnRonda4;
+    private JLabel lblJugador1;
+    private JLabel lblJugador2;
+    private JTextField jTextJugador1;
+    private JTextField jTextJugador2 ;
+    
+
+    
+    public PanelNombres(){
+        iniciarComponentes();
+    }
+    
+    private void iniciarComponentes(){
+        //Configuración de la ventana
+        setTitle("Tic Tac Toe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setVisible(true); 
+        setResizable(false);
+ 
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        
+        
+        //Configuración del Encabezado
+        
+        jpFondoNombres = new PanelConImagen("/inicioJuego.png"); 
+        jpFondoNombres.setSize(800,600);     
+        jpFondoNombres.setLayout(null);
+        add(jpFondoNombres);
+
+        
+      
+        ManejadorDeEventos manejadorEventos = new ManejadorDeEventos();
+        
+}
+    public void unJugador(){
+
+        lblJugador1 = new JLabel();
+        jTextJugador1 = new JTextField();
+        btnIniciar = new JButton("iniciar");
+        lblNumeroRondas = new JLabel();
+        btnRonda1 = new JButton();
+        btnRonda2 = new JButton();
+        btnRonda3 = new JButton();
+        btnRonda4 = new JButton();    
+        
+        
+        
+        ImageIcon icono = new ImageIcon("src/Jugador1.png");
+        lblJugador1.setIcon(icono);
+            
+        icono = new ImageIcon("src/rondas.png");
+        lblNumeroRondas.setIcon(icono);
+        
+        icono = new ImageIcon("src/1.png");
+        btnRonda1.setIcon(icono);
+        btnRonda1.setContentAreaFilled(false);
+
+
+        icono = new ImageIcon("src/2.png");
+        btnRonda2.setIcon(icono);
+        btnRonda2.setContentAreaFilled(false);
+            
+            
+        icono = new ImageIcon("src/3.png");
+        btnRonda3.setIcon(icono);
+        btnRonda3.setContentAreaFilled(false);
+            
+        icono = new ImageIcon("src/4.png");
+        btnRonda4.setIcon(icono);
+        btnRonda4.setContentAreaFilled(false);
+        
+        lblJugador1.setBounds(520,145, 259,83);    
+        jTextJugador1.setBounds(520,245, 250,44);
+        btnIniciar.setBounds(520,450, 250,44);
+        lblNumeroRondas.setBounds(540,300, 259,83);
+        btnRonda1.setBounds(540,380, 41,41);
+        btnRonda2.setBounds(595,380, 41,41);
+        btnRonda3.setBounds(650,380, 41,41);
+        btnRonda4.setBounds(705,380, 41,41);
+        
+        
+        jpFondoNombres.add(btnIniciar);
+        jpFondoNombres.add(lblJugador1);
+        jpFondoNombres.add(jTextJugador1);
+        jpFondoNombres.add(lblNumeroRondas);
+        jpFondoNombres.add(btnRonda1);
+        jpFondoNombres.add(btnRonda2);
+        jpFondoNombres.add(btnRonda3);
+        jpFondoNombres.add(btnRonda4);        
+        
+        
+        
+        
+        
+    }
+    public void dosJugadores(){
+        
+            btnRonda1 = new JButton();
+            btnRonda2 = new JButton();
+            btnRonda3 = new JButton();
+            btnRonda4 = new JButton();  
+            lblNumeroRondas = new JLabel();
+            lblJugador1 = new JLabel();
+            lblJugador2 = new JLabel();
+            jTextJugador1 = new JTextField();
+            jTextJugador2 = new JTextField();            
+            
+            
+            ImageIcon icono = new ImageIcon("src/Jugador1.png");
+            lblJugador1.setIcon(icono);
+                  
+            icono = new ImageIcon("src/Jugador2.png");
+            lblJugador2.setIcon(icono);
+            
+            icono = new ImageIcon("src/rondas.png");
+            lblNumeroRondas.setIcon(icono);
+            
+            icono = new ImageIcon("src/1.png");
+            btnRonda1.setIcon(icono);
+            btnRonda1.setContentAreaFilled(false);
+
+            
+            
+            icono = new ImageIcon("src/2.png");
+            btnRonda2.setIcon(icono);
+            btnRonda2.setContentAreaFilled(false);
+            
+            
+            icono = new ImageIcon("src/3.png");
+            btnRonda3.setIcon(icono);
+            btnRonda3.setContentAreaFilled(false);
+            
+            icono = new ImageIcon("src/4.png");
+            btnRonda4.setIcon(icono);
+            btnRonda4.setContentAreaFilled(false);
+
+            
+            lblJugador1.setBounds(520,45, 259,83);
+            lblJugador2.setBounds(520,215, 259,83);
+            
+            jTextJugador1.setBounds(520,125, 250,44);
+            jTextJugador2.setBounds(520,290, 250,44);
+            
+            lblNumeroRondas.setBounds(540,350, 259,83);
+            btnRonda1.setBounds(540,420, 41,41);
+            btnRonda2.setBounds(595,420, 41,41);
+            btnRonda3.setBounds(650,420, 41,41);
+            btnRonda4.setBounds(705,420, 41,41);
+            
+            btnIniciar = new JButton("iniciar Juego");
+            btnIniciar.setBounds(520,490, 250,44);
+
+            
+            jpFondoNombres.add(btnIniciar);
+            jpFondoNombres.add(lblJugador1);
+            jpFondoNombres.add(lblJugador2);
+            jpFondoNombres.add(jTextJugador2);
+            jpFondoNombres.add(jTextJugador1);
+            jpFondoNombres.add(lblNumeroRondas);
+            jpFondoNombres.add(btnRonda1);
+            jpFondoNombres.add(btnRonda2);
+            jpFondoNombres.add(btnRonda3);
+            jpFondoNombres.add(btnRonda4);
+    }
+      
+    
+            class ManejadorDeEventos implements ActionListener, KeyListener{
+        @Override
+        public void actionPerformed(ActionEvent evento){
+           
+            
+        
+        }
+        
+        @Override
+        public void keyReleased(KeyEvent e) {
+           /* System.out.println("Se liberó la tecla " + e.getKeyChar() +
+                    " Con codigo " + e.getKeyCode());*/
+
+        }
+        
+        @Override
+        public void keyPressed(KeyEvent e) {
+            /*System.out.println("Se presionó la tecla " + e.getKeyChar()+
+                    " Con codigo " + e.getKeyCode());*/
+            
+        }
+        
+        @Override
+        public void keyTyped(KeyEvent e) {
+            /*System.out.println("Se digitó la tecla " + e.getKeyChar()+
+                    " Con codigo " + e.getKeyCode());*/
+        }
+               
+    }
+           
+    
+}
---
+++ MiniProyecto2_1/src/Vista/VentanaJuego.java
@@ -0,0 +1,116 @@
+package Vista;
+
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author juandiego
+ */
+public class VentanaJuego extends JFrame{
+    private PanelConImagen jpFondoJuego;
+    private JLabel label1 ;
+    private JLabel label2 ;
+    private JLabel label3 ;
+    private JLabel label4 ;
+    private JLabel label5 ;
+    private JLabel label6 ;
+    private JLabel label7 ;
+    private JLabel label8 ;
+    private JLabel label9 ;
+    private JLabel lblRonda;
+    private JLabel lblNumRonda;
+    
+    
+    
+     public VentanaJuego(){
+        iniciarComponentes();
+    }
+    
+    private void iniciarComponentes(){
+        //Configuración de la ventana
+        setTitle("Tic Tac Toe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setVisible(true); 
+        setResizable(false);
+ 
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        
+        
+        //Configuración del Encabezado
+        jpFondoJuego = new PanelConImagen("/juego.png"); 
+        jpFondoJuego.setSize(800,600);     
+        jpFondoJuego.setLayout(null);
+        add(jpFondoJuego);
+        lblRonda = new JLabel();
+        label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
+        label4 = new JLabel();
+        label5 = new JLabel();
+        label6 = new JLabel();
+        label7 = new JLabel();
+        label8 = new JLabel();
+        label9 = new JLabel();
+        lblNumRonda = new JLabel();
+        
+        lblRonda.setBounds(500,30, 291,94);
+        ImageIcon icono = new ImageIcon("src/ronda.png");
+        lblRonda.setIcon(icono);
+
+        
+        lblNumRonda.setBounds(620,130, 41,41);
+        icono = new ImageIcon("src/1.png");
+        lblNumRonda.setIcon(icono);
+        
+        label1.setBounds(30,55, 145,145);
+        icono = new ImageIcon("src/cuadro.png");
+        label1.setIcon(icono);        
+        
+        label2.setBounds(191,55, 145,145);
+        label2.setIcon(icono);
+        
+        label3.setBounds(350,55, 145,145);
+        label3.setIcon(icono);
+        
+        label4.setBounds(30,213, 145,145);
+        label4.setIcon(icono);
+        
+        label5.setBounds(191,213, 145,145);
+        label5.setIcon(icono);
+        
+        label6.setBounds(350,213, 145,145);
+        label6.setIcon(icono);    
+
+        label7.setBounds(30,371, 145,145);
+        label7.setIcon(icono);
+        
+        label8.setBounds(191,371, 145,145);
+        label8.setIcon(icono);
+        
+        label9.setBounds(350,371, 145,145);
+        label9.setIcon(icono);
+        
+        
+        jpFondoJuego.add( label1);
+        jpFondoJuego.add( label2);
+        jpFondoJuego.add( label3);
+        jpFondoJuego.add( label4);
+        jpFondoJuego.add( label5);
+        jpFondoJuego.add( label6);
+        jpFondoJuego.add( label7);
+        jpFondoJuego.add( label8);
+        jpFondoJuego.add( label9);
+        jpFondoJuego.add( lblRonda);
+        jpFondoJuego.add( lblNumRonda);
+        
+ 
+        
+}
+    
+}
