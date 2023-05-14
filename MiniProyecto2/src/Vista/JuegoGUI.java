79f1befa099731197276d7de0b9a126dcec3fce6
Juan Camilo Capera <capera.juan@correounivalle.edu.co>
2023-05-14 04:27:31 -0500

    Creando la clase JuegoGUI

---
+++ MiniProyecto2/src/Vista/JuegoGUI.java
@@ -0,0 +1,19 @@
+/*
+ * MINIPROYECTO #2
+ *
+ * INTEGRANTES: 
+ * Juan Camilo Capera Barrera
+ * Juan Diego Castaño
+ * Grupo 01 FPOE 
+ *
+ * PROFESOR: Luis Yohany Romo Portilla 
+ */
+package Vista;
+
+/**
+ *
+ * @author juan-
+ */
+public class JuegoGUI {
+    
+}
