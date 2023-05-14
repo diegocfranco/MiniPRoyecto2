6fa6ce1b128b190d1593d8f6b122f72969fb0e8d
Juan Camilo Capera <capera.juan@correounivalle.edu.co>
2023-05-14 04:38:08 -0500

    configurando la logica del juego

--- MiniProyecto2/src/Logica/Juego.java
+++ MiniProyecto2/src/Logica/Juego.java
@@ -10,10 +10,52 @@
  */
 package Logica;
 
+import java.util.Calendar;
+
 /**
  *
  * @author juan-
  */
 public class Juego {
+    private int cantAciertos;
+    private int cantFallos;
+    private int numeroPartida;
+    private long tiempoJuego;
+    private int puntajeTotal;
     
+    public Juego(){
+        tiempoJuego = Calendar.getInstance().getTimeInMillis();
+    }
+    
+    public void actualizarDatos(boolean acerto) {
+        if (acerto) {
+            cantAciertos += 1;
+            puntajeTotal += 100;
+            numeroPartida += 1;
+        } else {
+            cantFallos += 1;
+            numeroPartida +=1;
+        }
+    }
+
+    public long getTiempoJuego() {
+        long tiempoActual = Calendar.getInstance().getTimeInMillis();
+        long tiempo = tiempoActual - tiempoJuego;
+        return tiempo;
+    }
+    public int getNumeroPartida(){
+        return numeroPartida;
+    }
+
+    public int getCantAciertos() {
+        return cantAciertos;
+    }
+
+    public int getCantFallos() {
+        return cantFallos;
+    }
+    
+    public int getPuntajeTotal() {
+        return puntajeTotal;
+    }
 }
