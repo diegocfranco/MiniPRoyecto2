86745a5fd151229e8b74af07b3d68e88128b0765
Juan Camilo Capera <capera.juan@correounivalle.edu.co>
2023-05-14 04:28:08 -0500

    Creando la clase Juego en el paquete Logica

---
+++ MiniProyecto2/src/Logica/Juego.java
@@ -0,0 +1,19 @@
+/*
+ * MINIPROYECTO #2
+ *
+ * INTEGRANTES: 
+ * Juan Camilo Capera Barrera
+ * Juan Diego Castaño
+ * Grupo 01 FPOE 
+ *
+ * PROFESOR: Luis Yohany Romo Portilla 
+ */
+package Logica;
+
+/**
+ *
+ * @author juan-
+ */
+public class Juego {
+    
+}
